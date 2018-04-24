d39244136449cba83962ffe7b1f65271712f3997
ytterbium <yann.balland@polytechnique.edu>
2018-04-24 19:29:12 +0200

    Correction du nom d'une colonne dans le traitement du .csv
    Ajout de deux scripts pour télécharger et archiver les csv

---
+++ archive.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+
+WD=$(pwd)
+BASENAME=$(readlink -f "$0")
+BASEDIR=$(dirname "$BASENAME")
+cd $BASEDIR
+
+FILENAME=$(date -u "+%Y-%m-%d.csv")
+cp faerix.csv $FILENAME
+zip archive_faerixiens.zip $FILENAME
+rm $FILENAME
+cd $WD
