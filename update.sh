a416fd2ef4b6eca72b2e0974ff12f56e05d53bca
Yann Balland <ytterbium@laposte.net>
2023-01-10 23:58:04 +0100

    Amélioration du script d'update

--- update.sh
+++ update.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-wget "https://docs.google.com/spreadsheets/d/10q0B4LbyfZUzZJfvLpx8goSOCg2TnU8gSuo3PEp-aV4/export?format=csv" -O faerix.csv
+#!/bin/sh
+
+WD=$(pwd)
+BASENAME=$(readlink -f "$0")
+BASEDIR=$(dirname "$BASENAME")
+cd $BASEDIR
+
+wget -q "https://docs.google.com/spreadsheets/d/10q0B4LbyfZUzZJfvLpx8goSOCg2TnU8gSuo3PEp-aV4/export?format=csv" -O faerix.csv
+
+cd $WD
